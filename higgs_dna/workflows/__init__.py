1e98bd66d2b438688a00fd034e18d5a5362faeb7
Caio Cesar Daumann <caio.cesar.daumann@cern.ch>
2024-07-18 13:08:11 +0200

    Zee processor with photon to electron matching

--- higgs_dna/workflows/__init__.py
+++ higgs_dna/workflows/__init__.py
@@ -8,6 +8,7 @@ from higgs_dna.workflows.particleLevel import ParticleLevelProcessor
 from higgs_dna.workflows.top import TopProcessor
 from higgs_dna.workflows.Zmmy import ZmmyProcessor, ZmmyHist, ZmmyZptHist
 from higgs_dna.workflows.hpc_processor import HplusCharmProcessor
+from higgs_dna.workflows.zee_processor import ZeeProcessor
 from higgs_dna.workflows.lowmass import lowmassProcessor
 
 workflows = {}
@@ -21,6 +22,7 @@ workflows["zmmy"] = ZmmyProcessor
 workflows["zmmyHist"] = ZmmyHist
 workflows["zmmyZptHist"] = ZmmyZptHist
 workflows["hpc"] = HplusCharmProcessor
+workflows["zee"] = ZeeProcessor
 workflows["lowmass"] = lowmassProcessor
 
 __all__ = ["workflows", "taggers", "DYStudiesProcessor"]
