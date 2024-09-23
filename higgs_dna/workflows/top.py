3a5fea4e656091e5f47b1ba8d665727610a73dd6
Florian Mausolf <florian.mausolf@rwth-aachen.de>
2024-09-23 17:31:42 +0200

    flow treatment of systematic variations updated

--- higgs_dna/workflows/top.py
+++ higgs_dna/workflows/top.py
@@ -186,6 +186,23 @@ class TopProcessor(HggBaseProcessor):  # type: ignore
         original_jets = events.Jet
         original_electrons = events.Electron
 
+        # Computing the normalizing flow correction
+        if self.data_kind == "mc" and self.doFlow_corrections:
+
+            logger.info("Calculating normalizing flow corrections to photon MVA ID inputs")
+            counts = ak.num(original_photons)
+            corrected_inputs,var_list = calculate_flow_corrections(original_photons, events, self.meta["flashggPhotons"]["flow_inputs"], self.meta["flashggPhotons"]["Isolation_transform_order"], year=self.year[dataset_name][0])
+
+            # Store the raw nanoAOD value and update photon ID MVA value for preselection
+            original_photons["mvaID_nano"] = original_photons["mvaID"]
+
+            # Store the raw values of the inputs and update the input values with the corrections since some variables used in the preselection
+            for i in range(len(var_list)):
+                original_photons["raw_" + str(var_list[i])] = original_photons[str(var_list[i])]
+                original_photons[str(var_list[i])] = ak.unflatten(corrected_inputs[:,i] , counts)
+
+            original_photons["mvaID"] = ak.unflatten(self.add_photonid_mva_run3(original_photons, events), counts)
+
         # systematic object variations
         for systematic_name in systematic_names:
             if systematic_name in available_object_systematics.keys():
@@ -286,25 +303,6 @@ class TopProcessor(HggBaseProcessor):  # type: ignore
             # recompute photonid_mva on the fly
             if self.photonid_mva_EB and self.photonid_mva_EE:
                 photons = self.add_photonid_mva(photons, events)
-
-            # Computing the normalizing flow correction
-            # align this with base workflow! Move flow corrections outside of loop!
-            if self.data_kind == "mc" and self.doFlow_corrections:
-
-                # Applying the Flow corrections to all photons before pre-selection
-                counts = ak.num(photons)
-                corrected_inputs,var_list = calculate_flow_corrections(photons, events, self.meta["flashggPhotons"]["flow_inputs"], self.meta["flashggPhotons"]["Isolation_transform_order"], year=self.year[dataset_name][0])
-
-                # Store the raw nanoAOD value and update photon ID MVA value for preselection
-                photons["mvaID_run3"] = ak.unflatten(self.add_photonid_mva_run3(photons, events), counts)
-                photons["mvaID_nano"] = photons["mvaID"]
-
-                # Store the raw values of the inputs and update the input values with the corrections since some variables used in the preselection
-                for i in range(len(var_list)):
-                    photons["raw_" + str(var_list[i])] = photons[str(var_list[i])]
-                    photons[str(var_list[i])] = ak.unflatten(corrected_inputs[:,i] , counts)
-
-                photons["mvaID"] = ak.unflatten(self.add_photonid_mva_run3(photons, events), counts)
 
             # photon preselection
             photons = photon_preselection(self, photons, events, year=self.year[dataset_name][0])
