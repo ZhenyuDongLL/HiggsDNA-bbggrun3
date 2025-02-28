cceb66686b7f53f732d7e3e3d8b34580ad260303
Jan Lukas Spah <janlukas.spaeh@physik.rwth-aachen.de>
2025-02-28 12:51:32 +0000

    Resolve "Remove fiducial taggers"

--- higgs_dna/workflows/taggers/__init__.py
+++ higgs_dna/workflows/taggers/__init__.py
@@ -5,8 +5,6 @@ from .DummyTagger2 import DummyTagger2
 from .HHWWggTagger import HHWWggTagger
 from .yTagger import yTagger
 from .ptTagger import ptTagger
-from .fiducialClassicalTagger import fiducialClassicalTagger
-from .fiducialGeometricTagger import fiducialGeometricTagger
 
 taggers: Dict[str, Any] = {}
 
@@ -15,7 +13,5 @@ taggers["DummyTagger2"] = DummyTagger2
 taggers["HHWWggTagger"] = HHWWggTagger
 taggers["yTagger"] = yTagger
 taggers["ptTagger"] = ptTagger
-taggers["fiducialClassicalTagger"] = fiducialClassicalTagger
-taggers["fiducialGeometricTagger"] = fiducialGeometricTagger
 
 __all__ = ["taggers"]
