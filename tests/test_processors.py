0b14deede2a5abf5cc8d303f377aada4c2a209d9
Nitish Kumar Kasaraguppe Veerappa Gowda <nitish.kumar.kasaraguppe.veerappa@cern.ch>
2025-08-22 14:11:26 +0200

    xRootD using X509 proxy service provided by CAT

--- tests/test_processors.py
+++ tests/test_processors.py
@@ -54,12 +54,11 @@ def test_processors(processor_class):
     subprocess.run("pull_files.py --target bTag", shell=True)
 
     # Need to pull some JSONs and ONNXs for HHbbggProcessor
-    # wget is used to download the files from the cernbox public link at the moment, should be updated with xrdcp
-    subprocess.run("wget -O higgs_dna/tools/WPs_btagging_HHbbgg.json https://cernbox.cern.ch/remote.php/dav/public-files/hOAABXExhgfL5AW/WPs_btagging.json", shell=True)
-    subprocess.run("wget -O higgs_dna/tools/Weights_interference_HHbbgg.json https://cernbox.cern.ch/remote.php/dav/public-files/ouOPOW9xLuJ4gM5/Weights_interference.json", shell=True)
-    subprocess.run("wget -O higgs_dna/tools/mjj_model_2023.onnx https://cernbox.cern.ch/remote.php/dav/public-files/ZfAj5DTMn4EPBAG/mjj_model_2023.onnx", shell=True)
-    subprocess.run("wget -O higgs_dna/tools/mjj_model_2022.onnx https://cernbox.cern.ch/remote.php/dav/public-files/UqXkR4Nte1mvBAr/mjj_model_2022.onnx", shell=True)
-    subprocess.run("wget -O higgs_dna/tools/HHbbgg_DNN_bpairing_allyears.onnx https://cernbox.cern.ch/remote.php/dav/public-files/2ckEwNSXbUg7okk/HHbbgg_DNN_bpairing_allyears.onnx", shell=True)
+    subprocess.run("xrdcp root://eoscms.cern.ch//eos/cms/store/group/phys_b2g/HHbbgg/nkasarag/HiggsDNA_JSONs/WPs_btagging.json higgs_dna/tools/WPs_btagging_HHbbgg.json", shell=True)
+    subprocess.run("xrdcp root://eoscms.cern.ch//eos/cms/store/group/phys_b2g/HHbbgg/nkasarag/HiggsDNA_JSONs/Weights_interference.json higgs_dna/tools/Weights_interference_HHbbgg.json", shell=True)
+    subprocess.run("xrdcp root://eoscms.cern.ch//eos/cms/store/group/phys_b2g/HHbbgg/jafan/mbbModels/mjj_model_2023.onnx higgs_dna/tools/mjj_model_2023.onnx", shell=True)
+    subprocess.run("xrdcp root://eoscms.cern.ch//eos/cms/store/group/phys_b2g/HHbbgg/jafan/mbbModels/mjj_model_2022.onnx higgs_dna/tools/mjj_model_2022.onnx", shell=True)
+    subprocess.run("xrdcp root://eoscms.cern.ch//eos/cms/store/group/phys_b2g/HHbbgg/ejourdhu/DNN_bpairing/DNN_bpairing_allyears.onnx higgs_dna/tools/HHbbgg_DNN_bpairing_allyears.onnx", shell=True)
 
     # Choose datasets to run over appropriately
     # In the future, should specify datasets on eos instead of local files
